10d6e6e6906f1647f2de8cbdbe69d6544c5b1619
Ariel <arielsvn@gmail.com>
2020-03-11 17:41:15 -0400

    deploy master

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const ApiHost = process.env.REACT_APP_API_HOST || 'https://api.refine.bio';
 
+// test
+
 module.exports = withImages(
   withSass(
     withBundleAnalyzer({
